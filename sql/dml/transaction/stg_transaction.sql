46c0b5ec994c504309c5d22f9db9de6691f20c28
Treglazov Egor <etreglazov@datanomica.org>
2025-06-04 09:13:22 +0300

    test

---
+++ sql/dml/transaction/stg_transaction.sql
@@ -0,0 +1,9 @@
+select
+    "123" as customer_id,
+    "COUNT(*)" as tx_count,
+    "SUM(amount)" as total_amount,
+    "AVG(amount)" as avg_amount,
+    "123" as customer_id_test,
+    "COUNT(*)" as tx_count_test,
+    "SUM(amount)" as total_amount_test,
+    "AVG(amount)" as _test  
